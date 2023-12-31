c751b2bbd59615f826752fd436fbf7678f5cd692
Felix <felixomosh7@gmail.com>
2024-01-01 01:00:21 +0300

    Completed assignent

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', () => alert('I was clicked!'))
 }
+addingEventListener();
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', addingEventListener);
+
+//input.addEventListener('click', () => alert('I was clicked'));
